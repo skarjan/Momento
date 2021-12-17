81684d06e48f15429ce6671d6ff86c068532f05b
Arjan <vanderveenarjan@gmail.com>
2021-12-17 09:33:58 +0100

    add animation to tagline

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@ function returnDateString() {
   let newDate = new Date();
   return newDate.getFullYear() + '-' + (newDate.getMonth() + 1) + '-' + newDate.getDate();
 }
-function returnTimeString(){
+
+function returnTimeString() {
   let newDate = new Date(),
-  adjustedMinutes = newDate.getMinutes();
-  if(adjustedMinutes<10){
-    adjustedMinutes = '0'+adjustedMinutes;
+    adjustedMinutes = newDate.getMinutes();
+  if (adjustedMinutes < 10) {
+    adjustedMinutes = '0' + adjustedMinutes;
   }
   let adjustedSecs = newDate.getSeconds();
-  if (adjustedSecs<10){
-    adjustedSecs = '0'+adjustedSecs;
+  if (adjustedSecs < 10) {
+    adjustedSecs = '0' + adjustedSecs;
   }
   return newDate.getHours() + ':' + adjustedMinutes + ':' + adjustedSecs;
 }
@@ -18,7 +19,14 @@ function returnTimeString(){
 
 console.log(returnDateString() + ' ' + returnTimeString());
 
-let p = document.getElementById('textContainer');
-setInterval(function(){
-  p.innerHTML = `${returnDateString()} ${returnTimeString()}`
+let clock = document.getElementById('clockContainer');
+let tagline = document.getElementById('textContainer');
+let ellipsis = document.getElementById('ellipsisContainer')
+setInterval(function () {
+  if (ellipsis.innerHTML.length > 2) {
+    ellipsis.innerHTML = '';
+  } else {
+    ellipsis.innerHTML = ellipsis.innerHTML + `.`
+  }
+  clock.innerHTML = `${returnDateString()} ${returnTimeString()}`;
 }, 1000)
