1fbcd4aebb3e2bed5b88a19229a98835d3e087b6
Arjan <vanderveenarjan@gmail.com>
2021-12-17 15:26:01 +0100

    small code refactor

--- js/main.js
+++ js/main.js
@@ -4,24 +4,27 @@ function returnDateString() {
 }
 
 function returnTimeString() {
+
   let newDate = new Date(),
-    adjustedMinutes = newDate.getMinutes();
+  adjustedMinutes = newDate.getMinutes();
   if (adjustedMinutes < 10) {
     adjustedMinutes = '0' + adjustedMinutes;
   }
+
   let adjustedSecs = newDate.getSeconds();
   if (adjustedSecs < 10) {
     adjustedSecs = '0' + adjustedSecs;
   }
+  
   return newDate.getHours() + ':' + adjustedMinutes + ':' + adjustedSecs;
 }
 
 
-console.log(returnDateString() + ' ' + returnTimeString());
 
-let clock = document.getElementById('clockContent');
-let tagline = document.getElementById('textContainer');
-let ellipsis = document.getElementById('ellipsisContainer')
+let clock = document.getElementById('clockContent'),
+  tagline = document.getElementById('textContainer'),
+  ellipsis = document.getElementById('ellipsisContainer');
+
 setInterval(function () {
   if (ellipsis.innerHTML.length > 2) {
     ellipsis.innerHTML = '';
