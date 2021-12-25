85abd70967cc780171a49d31e673a640d3215816
Arjan <vanderveenarjan@gmail.com>
2021-12-25 14:17:56 +0100

    refactor existing code

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function returnDateString() {
 function returnTimeString() {
 
   let newDate = new Date(),
-  adjustedMinutes = newDate.getMinutes();
+    adjustedMinutes = newDate.getMinutes();
   if (adjustedMinutes < 10) {
     adjustedMinutes = '0' + adjustedMinutes;
   }
@@ -15,21 +15,26 @@ function returnTimeString() {
   if (adjustedSecs < 10) {
     adjustedSecs = '0' + adjustedSecs;
   }
-  
+
   return newDate.getHours() + ':' + adjustedMinutes + ':' + adjustedSecs;
 }
 
 
+function timeClock() {
 
-let clock = document.getElementById('jsClock'),
-  tagline = document.getElementById('textContainer'),
-  ellipsis = document.getElementById('jsEllipsis');
+  let clock = document.getElementById('jsClock'),
+    tagline = document.getElementById('textContainer'),
+    ellipsis = document.getElementById('jsEllipsis');
 
-setInterval(function () {
-  if (ellipsis.innerHTML.length > 2) {
-    ellipsis.innerHTML = '';
-  } else {
-    ellipsis.innerHTML = ellipsis.innerHTML + `.`
-  }
-  clock.innerHTML = `${returnDateString()} ${returnTimeString()}`;
-}, 1000)
+  setInterval(function () {
+    if (ellipsis.innerHTML.length > 2) {
+      ellipsis.innerHTML = '';
+    } else {
+      ellipsis.innerHTML = ellipsis.innerHTML + `.`
+    }
+    clock.innerHTML = `${returnDateString()} ${returnTimeString()}`;
+  }, 1000)
+}
+
+// FuncMaster 
+timeClock()
